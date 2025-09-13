f00419e3c4daa8fa5ad8b50ae71fb67f33f9a4a3
Juan Sebastián <juansebastiansuescunh@gmail.com>
2025-09-13 00:15:27 -0500

    Versión final tablero pregunta 2

--- Tarea_5/Tablero_pregunta2.py
+++ Tarea_5/Tablero_pregunta2.py
@@ -5,6 +5,7 @@ from dash import dcc  # dash core components
 from dash import html # dash html components 
 from dash.dependencies import Input, Output
 import plotly.express as px
+import numpy as np
 
 #2. Importar datos
 df=pd.read_excel("Tarea_5/Datos_problema2.xlsx")
@@ -15,8 +16,10 @@ df_general= df.groupby(["location", "impact"]).size().unstack(fill_value=0).rese
 df_general=df_general[df_general['location']!='?']
 df_general['total_incidentes']=df_general['1 - High']+df_general['2 - Medium']+df_general['3 - Low']
 
-
-
+df_filtrado=df_general
+df_filtrado=df_filtrado.sort_values(by='total_incidentes',ascending=False)
+df_filtrado=df_filtrado.head(10)
+print(df_filtrado)
 
 #3.2  data frame totales
 total_high=df_general['1 - High'].sum()
@@ -34,8 +37,6 @@ dict_datos_totales={
     '2 - Medium':total_medium,
     '3 - Low':total_low
 }
-
-print(dict_datos_totales)
 
 df_totales=pd.DataFrame(datos_totales,columns=['Impacto','Total'])
 
@@ -86,23 +87,39 @@ app.layout = html.Div([
     ]),
     
     html.Div([
-        html.H1(children='Relación entre impacto y zona')
-    ]),
-
-    html.Div([
-
-        dcc.Graph(id='bar3'),
+        html.H1(children='Diseño de estrategia de cobertura'),
+        html.H3(children='Análisis absoluto, relativo y proporcional de incidentes por impacto y ubicación'),
+        html.P(children='Seleccione la visualización deseada:'),
 
         dcc.Dropdown(
             id='drop1',
-            options=['1 - High','2 - Medium','3 - Low'],
-            value='1 - High',
+            options=['Lineal','Logaritimica','Normalizada'],
+            value='Lineal',
             style={'width':'50%'}
         ),
 
-        dcc.Graph(id='pie2')
-    ])
+        dcc.Graph(id='bar3'),
 
+        html.H3(children='Cobertura absoluta y proporcional de la estrategia'),
+        html.P(children='Seleccione la visualización deseada:'),
+        dcc.Dropdown(
+            id='drop2',
+            options=['Pie chart','Bar plot'],
+            value='Pie chart',
+            style={'width':'50%'}
+        )
+    ]),
+
+
+    html.Div([
+        dcc.Graph(id='grafico1'),
+        dcc.Graph(id='grafico2'),
+        dcc.Graph(id='grafico3')    
+    ],style={
+    'display': 'grid',
+    'grid-template-columns': '1fr 1fr 1fr',
+    'gap': '10px'
+    }),
 
 
 ])
@@ -137,8 +154,9 @@ def update_figure(tamaño):
 
 @app.callback(
     Output('bar3', 'figure'),
-    [Input('slider1', 'value')])
-def update_figure(tamaño):
+    [Input('slider1', 'value'),
+     Input('drop1','value')])
+def update_figure(tamaño,tipo):
     df_filtrado=df_general
     df_filtrado=df_filtrado.sort_values(by='total_incidentes',ascending=False)
     df_filtrado=df_filtrado.head(tamaño)
@@ -151,43 +169,161 @@ def update_figure(tamaño):
         var_name="impacto",
         value_name="cantidad"
 )
+    if tipo=='Lineal':
+        fig=px.bar(
+            df_largo,
+            x='location',
+            y='cantidad',
+            barmode='stack',
+            color='impacto',
+            color_discrete_map=colores,
 
-    fig=px.bar(
+        )     
+
+    if tipo=='Logaritimica':
+        df_largo['ln_cantidad']=np.log(df_largo['cantidad'])
+        
+        fig=px.bar(
         df_largo,
         x='location',
-        y='cantidad',
-        barmode='stack',
+        y='ln_cantidad',
+        barmode='group',
         color='impacto',
         color_discrete_map=colores,
-
     )     
+    
+    if tipo=='Normalizada':
+        totales = df_largo.groupby("location")["cantidad"].transform("sum")
+        df_largo["porcentaje"] = df_largo["cantidad"] / totales * 100
+        fig = px.bar(
+            df_largo,
+            x="location",
+            y="porcentaje",      
+            color="impacto",
+            barmode="stack",
+            color_discrete_map={"1 - High": "red", "2 - Medium": "blue", "3 - Low": "green"},
+        )
+
     return fig                       
 
 @app.callback(
-    Output('pie2','figure'),
+    [Output('grafico1','figure'),
+     Output('grafico2','figure'),
+     Output('grafico3','figure')],
     [Input('slider1','value'),
-     Input('drop1','value')]
+     Input('drop2','value')]
 )
 def update_figure(tamaño,tipo):
     df_filtrado=df_general
     df_filtrado=df_filtrado.sort_values(by='total_incidentes',ascending=False)
     df_filtrado=df_filtrado.head(tamaño)
-    suma=df_filtrado[tipo].sum()
-    totales=dict_datos_totales[tipo]
-    cubierto=suma
-    p_cubierto=cubierto/totales
-    no_cubierto=1-p_cubierto
-    datos_porcentajes=[
-        ['cubierto',p_cubierto],
-        ['no cubierto',no_cubierto]
-        ]
-    df_porcentajes=pd.DataFrame(datos_porcentajes,columns=['Cobertura','Porcentajes'])
-    fig=px.pie(
-        df_porcentajes,
-        values='Porcentajes',
-        names='Cobertura'
-    )
-    return fig
+    suma_high=df_filtrado['1 - High'].sum()
+    suma_medium=df_filtrado['2 - Medium'].sum()
+    suma_low=df_filtrado['3 - Low'].sum()
+
+    if tipo=='Pie chart':
+        cubierto_high=suma_high/dict_datos_totales['1 - High']
+        no_cubierto_high=1-cubierto_high
+
+        cubierto_medium=suma_medium/dict_datos_totales['2 - Medium']
+        no_cubierto_medium=1-cubierto_medium
+
+        cubierto_low=suma_low/dict_datos_totales['3 - Low']
+        no_cubierto_low=1-cubierto_low
+
+        datos_high=[
+            ['cubierto',cubierto_high],
+            ['no cubierto',no_cubierto_high]
+            ]
+        df_high=pd.DataFrame(datos_high,columns=['Cobertura','Porcentajes'])
+
+        datos_medium=[
+            ['cubierto',cubierto_medium],
+            ['no cubierto',no_cubierto_medium]
+            ]
+        df_medium=pd.DataFrame(datos_medium,columns=['Cobertura','Porcentajes'])
+
+        datos_low=[
+            ['cubierto',cubierto_low],
+            ['no cubierto',no_cubierto_low]
+            ]
+        df_low=pd.DataFrame(datos_low,columns=['Cobertura','Porcentajes'])
+
+        fig1=px.pie(
+            df_high,
+            values='Porcentajes',
+            names='Cobertura',
+            color='Cobertura',
+            title='Cobertura incidentes de impacto alto'
+        )
+
+        fig2=px.pie(
+            df_medium,
+            values='Porcentajes',
+            names='Cobertura',
+            color='Cobertura',
+            title='Cobertura incidentes de impacto medio'
+        )
+
+        fig3=px.pie(
+            df_low,
+            values='Porcentajes',
+            names='Cobertura',
+            color='Cobertura',
+            title='Cobertura incidentes de impacto bajo'
+        )
+    if tipo=='Bar plot':
+        cubierto_high=suma_high
+        no_cubierto_high=dict_datos_totales['1 - High']-cubierto_high
+
+        cubierto_medium=suma_medium
+        no_cubierto_medium=dict_datos_totales['2 - Medium']-cubierto_medium
+
+        cubierto_low=suma_low
+        no_cubierto_low=dict_datos_totales['3 - Low']-cubierto_low
+
+        datos_high=[
+            ['cubierto',cubierto_high],
+            ['no cubierto',no_cubierto_high]
+            ]
+        df_high=pd.DataFrame(datos_high,columns=['Cobertura','Cantidades'])
+
+        datos_medium=[
+            ['cubierto',cubierto_medium],
+            ['no cubierto',no_cubierto_medium]
+            ]
+        df_medium=pd.DataFrame(datos_medium,columns=['Cobertura','Cantidades'])
+
+        datos_low=[
+            ['cubierto',cubierto_low],
+            ['no cubierto',no_cubierto_low]
+            ]
+        df_low=pd.DataFrame(datos_low,columns=['Cobertura','Cantidades'])
+
+        fig1 = px.bar(
+            df_high,
+            x="Cobertura",
+            y="Cantidades",
+            color='Cobertura',
+            title='Cobertura incidentes de impacto alto'      
+        )
+
+        fig2 = px.bar(
+            df_medium,
+            x="Cobertura",
+            y="Cantidades",
+            color='Cobertura',
+            title='Cobertura incidentes de impacto medio'      
+        )
+
+        fig3 = px.bar(
+            df_low,
+            x="Cobertura",
+            y="Cantidades",
+            color='Cobertura',
+            title='Cobertura incidentes de impacto bajo'      
+        )
+    return fig1, fig2, fig3
 
 
 if __name__ == '__main__':
